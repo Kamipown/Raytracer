5d55308493c2f1e2aac5ba88c7203e2445460b82
dcognata <dcognata@student.42.fr>
2016-09-10 18:42:54 +0200

    cut all sdl part in sdl_init and sdl_loop

--- sources/main.c
+++ sources/main.c
@@ -20,39 +20,26 @@ static void	check(int argc)
 		error(-2, "Missing file.");
 }
 
+void		sdl_init(t_env *e)
+{
+	if ((e->sdl_win = SDL_CreateWindow("RT - 42", SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED, e->scene->size.w, e->scene->size.h,
+		SDL_WINDOW_RESIZABLE)) == NULL)
+		error(-20, (char *)SDL_GetError());
+}
+
 void		sdl_loop(t_env *e)
 {
-	t_boolean quit = FALSE;
+	t_boolean quit;
 
-	e->sdl_win = NULL;
-	e->sdl_win = SDL_CreateWindow(
-		"RT - 42",                  // window title
-		SDL_WINDOWPOS_UNDEFINED,           // initial x position
-		SDL_WINDOWPOS_UNDEFINED,           // initial y position
-		e->scene->size.w,                               // width, in pixels
-		e->scene->size.h,                               // height, in pixels
-		SDL_WINDOW_OPENGL                  // flags - see below
-	);
-	if(e->sdl_win)
+	quit = FALSE;
+	while (quit == FALSE)
 	{
-		while (quit == FALSE)
-		{
-			SDL_WaitEvent(&e->event);
-			switch(e->event.type) {
-				case SDL_KEYDOWN:
-					switch(e->event.key.keysym.sym) {
-					case SDLK_ESCAPE:
-						quit = TRUE;
-						break;
-					}
-					break;
-			}
-		}
-		SDL_DestroyWindow(e->sdl_win);
+		SDL_WaitEvent(&e->event);
+		if (e->event.type == SDL_KEYDOWN)
+			if (e->event.key.keysym.sym == SDLK_ESCAPE)
+				quit = TRUE;
 	}
-	else
-		error(-20, (char *)SDL_GetError());
-	SDL_Quit();
 }
 
 int			main(int argc, char *argv[])
@@ -63,9 +50,12 @@ int			main(int argc, char *argv[])
 	if (!(e = (t_env *)malloc(sizeof(t_env))))
 		error(-3, "Unable to initialize environment.");
 	init(e, argv[1]);
-	if (SDL_Init(SDL_INIT_VIDEO) != 0 )
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
 		error(-20, (char *)SDL_GetError());
+	sdl_init(e);
 	sdl_loop(e);
+	SDL_DestroyWindow(e->sdl_win);
+	SDL_Quit();
 	mlx_expose_hook(e->win, expose_hook, e);
 	mlx_hook(e->win, 2, (1L << 0), key_hook, e);
 	mlx_loop(e->mlx);
