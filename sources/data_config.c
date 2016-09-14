8b8b78bd432027a409107105bf4f5e7b5eac353b
OpheliePS <opettex-@student.42.fr>
2016-09-14 17:45:31 +0200

    remove printf

--- sources/data_config.c
+++ sources/data_config.c
@@ -64,5 +64,4 @@ void		fill_screen_data(t_scene *s, char *data)
 	s->size.s_y = -s->size.h / 2;
 	s->size.e_x = s->size.w / 2;
 	s->size.e_y = s->size.h / 2;
-	printf("%d %d | %d %d | %d %d\n", s->size.w, s->size.h, s->size.s_x, s->size.s_y, s->size.e_x, s->size.e_y);
 }
