93074908e0d71e61a60e23beb3e29642b021ebf7
dcognata <dcognata@student.42.fr>
2016-09-21 14:21:25 +0200

    cam in second function

--- sources/raytracer.c
+++ sources/raytracer.c
@@ -6,7 +6,7 @@
 /*   By: pdelobbe <pdelobbe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/03 19:40:21 by pdelobbe          #+#    #+#             */
-/*   Updated: 2016/09/20 17:33:13 by dcognata         ###   ########.fr       */
+/*   Updated: 2016/09/21 14:21:04 by dcognata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,45 +24,60 @@ static void	throw_ray(t_env *e, t_ray *ray)
 	// 	return ;
 }
 
-void		raytrace(t_env *e)
+void		raytrace2(t_env *e)
 {
 	int		x;
 	int		y;
-	t_ray	*ray;
 
-	t_vec3	veca;
-	t_vec3	vecb;
-	t_vec3	vecdir;
-	t_vec3	khey; // (ray)
-	veca.x = 0;
-	veca.y = 0;
-	veca.z = 0;
+	t_vec3 origin;
+	t_vec3 dir;
+	t_vec3 b;
+	origin.x = 0;
+	origin.y = 0;
+	origin.z = 0;
 
-	ray = init_ray();
 	y = 0;
 	while (y < e->scene->size.h)
 	{
 		x = 0;
 		while (x < e->scene->size.w)
 		{
-			//point visay
-			vecb.x = (e->scene->size.w / 2) - x;
-			vecb.y = (e->scene->size.h / 2) - y;
-			vecb.z = (2 * tan(e->scene->cam->fov / 2) / e->scene->size.w) * -1;
-			vecb = vector_normalize(&vecb);
-			vecdir.x = vecb.x - veca.x;
-			vecdir.y = vecb.y - veca.y;
-			vecdir.z = vecb.z - veca.z;
-			vecdir = vector_normalize(&vecdir);
-			printf("vecdir x-%f y-%f z-%f\n", vecdir.x, vecdir.y, vecdir.z);
+			b.x = (e->scene->size.w / 2) - x;
+			b.y = (e->scene->size.h / 2) - y;
+			b.z = 640 / e->scene->cam->fov * -1;
+			b = vector_normalize(&b);
 
-			exit(0);
-			update_ray(ray, x, y);
-			throw_ray(e, ray);
+			dir.x = b.x - origin.x;
+			dir.y = b.y - origin.y;
+			dir.z = b.z - origin.z;
+			dir = vector_normalize(&dir);
 			++x;
 		}
 		++y;
 	}
-	ft_putendl("raytrace");
-	free(ray);
+}
+
+
+void		raytrace(t_env *e)
+{
+	raytrace2(e);
+	// int		x;
+	// int		y;
+	// t_ray	*ray;
+	//
+	// ray = init_ray();
+	// y = 0;
+	// while (y < e->scene->size.h)
+	// {
+	// 	x = 0;
+	// 	while (x < e->scene->size.w)
+	// 	{
+	// 		update_ray(ray, x, y);
+	// 		throw_ray(e, ray);
+	// 		++x;
+	// 	}
+	// 	++y;
+	// }
+	// ft_putendl("raytrace");
+	// free(ray);
 }
