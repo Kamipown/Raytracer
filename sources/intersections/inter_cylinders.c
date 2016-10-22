0ce845902a37c3ab7838d2cf2a6daf94e620ed84
OpheliePS <opettex-@student.42.fr>
2016-10-22 19:24:09 +0200

    first cylinders

--- sources/intersections/inter_cylinders.c
+++ sources/intersections/inter_cylinders.c
@@ -14,7 +14,31 @@
 
 t_bool	inter_cylinders(t_ray *ray, t_obj *c, double *t)
 {
-	if (ray && c && t)
+	//if (ray && c && t)
+	//	return (FALSE);
+	//return (FALSE);
+
+	t_equation	e;
+	t_vec3 		l;
+	double 		x;
+	double		y;
+
+	c->rot = (t_vec3){0, 1, 0};
+	l = vec_sub(ray->origin, c->pos);
+	x = vec_mul_to_d(ray->dir, c->rot);
+	y = vec_mul_to_d(l, c->rot);
+	e.a = vec_mul_to_d(ray->dir, ray->dir) - (x * x);
+	e.b = 2 * (vec_mul_to_d(ray->dir, l) - (x * y));
+	e.c = vec_mul_to_d(l, l) - (c->radius * c->radius);
+
+	e.delta = e.b * e.b - 4 * e.a * e.c;
+	if (e.delta >= 0)
+	{
+			*t = e.z1 = (-e.b + sqrt(e.delta)) / (2.0 * e.a);
+			*t = e.z2 = (-e.b - sqrt(e.delta)) / (2.0 * e.a);
+			return ((e.z1 > e.z2) ? e.z1 : e.z2);
+
+	}
+	else
 		return (FALSE);
-	return (FALSE);
 }
