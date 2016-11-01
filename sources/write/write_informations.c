0b62e3b4a75c067d94fb349321d75b115cd94ba6
DCOGNATA <dcognata@student.42.fr>
2016-11-01 22:40:37 +0100

    retry

--- sources/write/write_informations.c
+++ sources/write/write_informations.c
@@ -16,31 +16,29 @@ void			write_informations(t_env *e)
 {
     TTF_Init();
 	if(TTF_Init() == -1)
-	    error(-25, "Unable to initialize environment.");
+	    error(-25, "Unable to initialize fonts environment.");
+
+	//
 	TTF_Font *police = NULL;
 	SDL_Rect position;
 	position.x = 0;
 	position.y = 0;
-	police = TTF_OpenFont("./resources/fonts/Oswald-Bold.ttf", 100);
+	police = TTF_OpenFont("./resources/fonts/Oswald-Bold.ttf", 10);
 
 	SDL_Color White = {255, 255, 255, 1};
-	SDL_Surface* surface_message = TTF_RenderText_Solid(police, "PD TOUS DES PUTAINS DE PD", White);
+	SDL_Surface* surface_message = TTF_RenderText_Solid(police, "PD TOUS DES", White);
 	SDL_Texture* message = SDL_CreateTextureFromSurface(e->renderer, surface_message);
 	SDL_Rect message_rect;
 	message_rect.x = 0; 
 	message_rect.y = 0;
-	message_rect.w = 300;
-	message_rect.h = 50;
+	message_rect.w = 80;
+	message_rect.h = 20;
 	SDL_RenderCopy(e->renderer, message, NULL, &message_rect);
 	
-    time_t rawtime;
-    struct tm * timeinfo;
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
-	// SDL_SaveBMP(surface_message, ft_strjoin(ft_strjoin(ft_itoa(timeinfo->tm_min), ft_itoa(timeinfo->tm_sec)), ".png"));
-
 	SDL_DestroyTexture(message);
 	SDL_FreeSurface(surface_message);
+	
+	//
 	TTF_CloseFont(police);
 	TTF_Quit();
 }
