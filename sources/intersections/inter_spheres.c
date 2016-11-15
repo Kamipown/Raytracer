59b3e7262f204da143f75344fd515e314e108bb6
Valentin MONTAGNOLI <vmontagn@e2r6p17.42.fr>
2016-11-15 01:47:22 +0100

    edit rotation error

--- sources/intersections/inter_spheres.c
+++ sources/intersections/inter_spheres.c
@@ -6,26 +6,32 @@
 /*   By: pdelobbe <pdelobbe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/14 14:42:48 by pdelobbe          #+#    #+#             */
-/*   Updated: 2016/10/28 00:56:17 by pdelobbe         ###   ########.fr       */
+/*   Updated: 2016/11/14 23:28:03 by vmontagn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
 
-t_bool	inter_spheres(t_ray *ray, t_obj *s, double *t)
+t_bool			inter_spheres(t_ray *ray, t_obj *s, double *t)
 {
-	t_equation e;
+	t_equation	e;
+	t_vec3		tmp_dir;
+	t_vec3		p;
 
-	e.a = ray->dir.x * ray->dir.x
-		+ ray->dir.y * ray->dir.y
-		+ ray->dir.z * ray->dir.z;
-	e.b = ray->dir.x * (ray->origin.x - s->pos.x)
-		+ ray->dir.y * (ray->origin.y - s->pos.y)
-		+ ray->dir.z * (ray->origin.z - s->pos.z);
+	tmp_dir = (t_vec3){ray->dir.x, ray->dir.y, ray->dir.z};
+	p = vec_sub(ray->origin, s->pos);
+	vec_rotate(&p, s->rot);
+	vec_rotate(&tmp_dir, s->rot);
+	e.a = tmp_dir.x * tmp_dir.x
+		+ tmp_dir.y * tmp_dir.y
+		+ tmp_dir.z * tmp_dir.z;
+	e.b = tmp_dir.x * p.x
+		+ tmp_dir.y * p.y
+		+ tmp_dir.z * p.z;
 	e.b *= 2.0;
-	e.c = (ray->origin.x - s->pos.x) * (ray->origin.x - s->pos.x)
-		+ (ray->origin.y - s->pos.y) * (ray->origin.y - s->pos.y)
-		+ (ray->origin.z - s->pos.z) * (ray->origin.z - s->pos.z);
+	e.c = p.x * p.x
+		+ p.y * p.y
+		+ p.z * p.z;
 	e.c -= (s->radius * s->radius);
 	return (solve_quadratic(e, t));
 }
